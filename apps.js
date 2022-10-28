e10bce7b85aa45f24f8dd5ba24c0320d617f1c98
NoobKilla5412 <brandoncarlrobertson@gmail.com>
2022-10-27 19:00:51 -0700

    Update

---
+++ apps.js
@@ -0,0 +1,64 @@
+const games = {
+  bird: {
+    title: "Bird",
+    id: "Bird",
+    image: "image/bird.svg",
+    version: "1.6"
+  },
+  europe: {
+    title: "Europe Takeover",
+    id: "EuropeTakeover",
+    image: "image/europe.svg",
+    version: "0.1"
+  },
+  goldMiner: {
+    title: "Gold Miner",
+    id: "GoldMiner",
+    image: "image/goldMiner.svg",
+    version: "0.6"
+  },
+  idleTaco: {
+    title: "Idle Taco",
+    id: "IdleTaco",
+    image: "image/taco.svg",
+    version: "1.7.3"
+  },
+  tacoClicker: {
+    title: "Taco Clicker",
+    id: "TacoClicker",
+    image: "image/taco.svg",
+    version: "1.2"
+  }
+};
+const defaultApps = {
+  update: {
+    title: "Update",
+    image: "image/update.svg"
+  },
+  info: {
+    title: "Info",
+    image: "image/info.svg",
+    link: "info.html"
+  },
+  appStore: {
+    title: "App Store",
+    image: "image/appStore.svg",
+    link: "appStore.html"
+  }
+};
+const apps = {
+  github: {
+    title: "GitHub",
+    image: "https://github.com/fluidicon.png",
+  },
+  google: {
+    title: "Google",
+    image: "https://www.google.com/images/branding/googleg/1x/googleg_standard_color_128dp.png",
+    link: "https://www.google.com/webhp?igu=1"
+  },
+  wikipedia: {
+    title: "Wikipedia",
+    image: "",
+    link: ""
+  }
+};
