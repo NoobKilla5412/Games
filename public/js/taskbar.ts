4c9bf4667c6ea3f6e07ac76a19b1861f83828170
NoobKilla5412 <92069057+NoobKilla5412@users.noreply.github.com>
2023-02-08 17:06:58 -0800

    Add Games

--- public/js/taskbar.ts
+++ public/js/taskbar.ts
@@ -52,6 +52,34 @@ const apps = {
   Bird: {
     link: () => `/games/Bird/index.html`,
     icon: "/image/bird.svg"
+  },
+  "Emoji Match": {
+    link: () => `/games/Emoji Match/index.html`,
+    icon: "/image/emojiMatch.svg"
+  },
+  "Europe Takeover": {
+    link: () => `/games/Europe Takeover/index.html`,
+    icon: "/image/europe.svg"
+  },
+  "Gold Miner": {
+    link: () => `/games/Gold Miner/index.html`,
+    icon: "/image/goldMiner.svg"
+  },
+  "Idle Taco": {
+    link: () => `/games/Idle Taco/index.html`,
+    icon: "/image/taco.svg"
+  },
+  "Idle Taco 2": {
+    link: () => `/games/Idle Taco 2/index.html`,
+    icon: "/image/taco.svg"
+  },
+  "Pig Farmer": {
+    link: () => `/games/Pig Farmer/index.html`,
+    icon: "/image/pigFarmer.svg"
+  },
+  "Taco Clicker": {
+    link: () => `/games/Taco Clicker/index.html`,
+    icon: "/image/taco.svg"
   }
 };
 
